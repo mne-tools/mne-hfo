e32909925abdc409ff9a45f6f537acfd3ff1afb4
Rachel June Smith <rachel.june.smith1@gmail.com>
2021-04-14 09:22:48 -0400

    initial commit

--- mne_hfo/detect.py
+++ mne_hfo/detect.py
@@ -282,6 +282,12 @@ class RMSDetector(Detector):
         Offset which is added to the final detection. This is used when the
         function is run in separate windows. Default = 0
 
+    Notes
+    ----------
+
+    TODO later
+
+    
     References
     ----------
     [1] R. J. Staba, C. L. Wilson, A. Bragin, I. Fried, and J. Engel,
